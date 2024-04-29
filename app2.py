7e804dba53c6bee5e9d5994e5a83e56715383ac6
Knust06 <lucasknust0605@gmail.com>
2024-04-29 14:43:14 -0300

    Adicionando melhor retorno em classes cardapio e criando objetos no app2.py

--- app2.py
+++ app2.py
@@ -1,17 +1,24 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.bebida import Bebida
 
 restaurante_praca = Restaurante('Praça','Comida Brasileira')
-restaurante_praca.receber_avaliacao('João', 5)
-restaurante_praca.receber_avaliacao('Maria', 4.3)
-restaurante_praca.receber_avaliacao('José', 9)
+#restaurante_praca.receber_avaliacao('João', 5)
+#restaurante_praca.receber_avaliacao('Maria', 4.3)
+#restaurante_praca.receber_avaliacao('José', 9)
 #restaurante_praca.alternar_estados = True
 #restaurante_pizza = Restaurante('Pizza Hut','Pizzaria')
 #restaurante_japones = Restaurante('Sushi House','Japonesa')
+bebida_coca = Bebida('Coca-Cola', 5.5, '600ml')
+bebida_suco = Bebida('Suco de Laranja', 4.5, '300ml')
+
+prato_pao = Prato('Pão de Queijo', 3.5, 'Pão de queijo mineiro')
 
 
 
 def main():
-    Restaurante.listar_restaurantes()
+    #Restaurante.listar_restaurantes()
+    pass
 
 
 if __name__ == '__main__':
